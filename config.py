6fa576efa19d5446233811a9cc85b43c787aff13
bezbak <Cemetey.2019@gmail.com>
2025-07-26 18:17:05 +0600

    add admin

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ BOT_TOKEN = env.str("TELEGRAM_BOT_TOKEN")  # Замените на токен в
 REGISTRATION_URL = 'https://1wqvwb.life/casino/list?open=register&p=r6w8'  # Ссылка на сайт регистрации
 MINI_APP_URL = 'https://yourwebsite.com/miniapp'  # Ссылка на мини-приложение
 INSTRUCTION_TEXT = 'Это текст инструкции. Замените на ваш текст.'  # Замените на вашу инструкцию
-ADMIN_USERNAME = '@awbsou'  # ID чата админа
+ADMIN_USERNAME = '@teamonebln_support'  # ID чата админа
 WELCOME_IMAGE = "images/main.png"
 ADMIN_ID = 7492044907
